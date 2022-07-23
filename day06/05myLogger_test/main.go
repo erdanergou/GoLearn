79b5df81f9e7bec5dd34527e93296c14f8941678
erdanergou <p17686264821@163.com>
2022-07-23 16:32:40 +0800

    2022/7/23 16:32

--- day06/05myLogger_test/main.go
+++ day06/05myLogger_test/main.go
@@ -5,8 +5,8 @@ import "GoLearn/day06/04myLogger"
 // 测试自建日志库
 func main() {
 	// logger := myLogger.NewLog("Error")
-	logger := myLogger.NewFileLogger("Info", "./", "test.log", 10*1024*1024)
-	n := 0
+	logger := myLogger.NewFileLogger("Info", "./", "test.log", 10*1024)
+	// n := 0
 	for {
 		logger.Debug("这是一条Debug日志")
 		logger.Info("这是一条Info日志")
@@ -15,9 +15,9 @@ func main() {
 		str := "发送了垃圾"
 		logger.Error("这是一条Error日志,id:%d,str:%s", id, str)
 		logger.Fatal("这是一条Fatal日志")
-		n++
-		if n >= 10{
-			break
-		}
+		// n++
+		// if n >= 10{
+		// 	break
+		// }
 	}
 }
